e94e3ee37dbe579458a541912a137ae2a3725f86
guanerye <lisongnan7@hotmail.com>
2017-05-28 20:52:05 +0900

    2017

---
+++ test01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+for num in range(10,20):
+    print 'num is : ================== ', num, '  start'
+
+    for i in range(2,num):
+        #print 'i is: ', i
+        if num%i==0:
+            j = num/i
+            print '%d 等于%d * %d' % (num,i,j)
+            break
+
+    else:
+        print num, '是一个质数'
+
+    print 'num is : ================== ', num, '  stop '
+    print '\n'
+
+
+
+
