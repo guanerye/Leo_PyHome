f0096d1905df3420a7c469177082cf760cbea5a5
guanerye <lisongnan7@hotmail.com>
2017-05-30 09:59:30 +0900

    20170530

--- test01.py
+++ test01.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-for num in range(10,20):
-    print 'num is : ================== ', num, '  start'
+def printme(str):
+    print str
+    return
 
-    for i in range(2,num):
-        #print 'i is: ', i
-        if num%i==0:
-            j = num/i
-            print '%d 等于%d * %d' % (num,i,j)
-            break
-
-    else:
-        print num, '是一个质数'
-
-    print 'num is : ================== ', num, '  stop '
-    print '\n'
-
-
-
-
+printme("fuck")
